439b8eb99772d7a93d23b266775b3d5a973e0a29
togawa427 <95838999+togawa427@users.noreply.github.com>
2024-02-01 17:09:03 +0900

    feat: データベース定義した

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 	"time"
@@ -92,20 +91,103 @@ func SetUpDatabase() {
 	}
 	defer db.Close()
 
-	sqlStmt:= `
-	create table products (id integer not null primary key, name text);
-	delete from foo;
+	// productsテーブルの作成
+	cmd := `
+	CREATE TABLE IF NOT EXISTS products (
+		id INTEGER NOT NULL PRIMARY KEY,
+		name TEXT NOT NULL,
+		barcode INTEGER UNIQUE,
+		price INTEGER,
+		stock INTEGER
+	);
 	`
-	_, err = db.Exec(sqlStmt)
+	_, err = db.Exec(cmd)
 	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		fmt.Println("強制終了")
+		log.Printf("failed create products table %q: %s\n", err, cmd)
 		return
 	}
 
+	// assetsテーブルの作成
+	cmd = `
+	CREATE TABLE IF NOT EXISTS assets (
+		id INTEGER NOT NULL PRIMARY KEY,
+		money INTEGER,
+		debt INTEGER
+	);
+	`
+	_, err = db.Exec(cmd)
+	if err != nil {
+		log.Printf("failed create assets table %q: %s\n", err, cmd)
+		return
+	}
+
+	// paymentsテーブルの作成
+	cmd = `
+	CREATE TABLE IF NOT EXISTS payments (
+		id INTEGER NOT NULL PRIMARY KEY,
+		price INTEGER,
+		date DATETIME,
+		method TEXT
+	);
+	`
+	_, err = db.Exec(cmd)
+	if err != nil {
+		log.Printf("failed create payments table %q: %s\n", err, cmd)
+		return
+	}
+
+	// payment_productsテーブルの作成
+	cmd = `
+	CREATE TABLE IF NOT EXISTS payment_products (
+		id INTEGER NOT NULL PRIMARY KEY,
+		payment_id INTEGER,
+		product_id INTEGER,
+		quantity INTEGER,
+		unit_price INTEGER
+	);
+	`
+	_, err = db.Exec(cmd)
+	if err != nil {
+		log.Printf("failed create payment_products table %q: %s\n", err, cmd)
+		return
+	}
+
+	// arrivalsテーブルの作成
+	cmd = `
+	CREATE TABLE IF NOT EXISTS arrivals (
+		id INTEGER NOT NULL PRIMARY KEY,
+		money INTEGER,
+		date DATETIME
+	);
+	`
+	_, err = db.Exec(cmd)
+	if err != nil {
+		log.Printf("failed create arrivals table %q: %s\n", err, cmd)
+		return
+	}
+
+	// arrival_productsテーブルの作成
+	cmd = `
+	CREATE TABLE IF NOT EXISTS arrival_products (
+		id INTEGER NOT NULL PRIMARY KEY,
+		arrival_id INTEGER,
+		product_id INTEGER,
+		quantity INTEGER
+	);
+	`
+	_, err = db.Exec(cmd)
+	if err != nil {
+		log.Printf("failed create arrival_products table %q: %s\n", err, cmd)
+		return
+	}
+
+
 	// データ挿入
-	_, err = db.Exec("insert into foo(id, name) values(1, 'foo'), (2, 'bar'), (3, 'baz')")
+	cmd = "INSERT INTO products (name, barcode, price, stock) VALUES (?, ?, ?, ?);"
+	_, err = db.Exec(cmd, "じゃがりこサラダ味", 134912341234, 120, 9)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	
 }
